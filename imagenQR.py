96daa97f9f525de8da1a3a5b14690c0c87c2c61a
Juan Pablo de León Miranda <JuanPablodeLeon>
2025-11-27 01:30:06 -0600

    imagenQR listo para QRs

--- imagenQR.py
+++ imagenQR.py
@@ -1,7 +1,45 @@
 import qrcode
 
-url = input("Enter URL: ").strip()
-file_path = "E:\\juanp\\Documents\\qrcode.png"
+nombres1 = ["Lic_Juan_Ramon_y_fam", "Gloria_Shaul", "Ing_Andrea", "Ing_Rodolfo", "Lic_Gabriela"]
+nombres2 = ["Maria_Elena_Shaul", "Sr_Leonardo", "Profe_Rolando", "Lic_Saul", "Inge_Rolando"]
+nombres3 = ["Lic_Manolita", "Profa_Karla", "Sra_Marcela", "Profa_Eunice", "Ing_Ivan"]
+nombres4 = ["Prof_Bernardo", "Anita", "Ing_Miguel", "Javier", "Julio"]
+nombres5 = ["Prof_Roberto", "Sharon", "Ing_Mario", "Profa_Anita", "Rony"]
+nombres6 = ["Ing_Carlos", "Scarleth", "Liliana", "Ing_Herman", "Profa_Magnolia"]
+nombres7 = ["Lica_Dedy", "Lica_Evelyn", "Sra_Elena", "Ing_Jovany"]
+
+""" 
+nombreArchivoHTML=["LicJuanRamonLopezyfam","GloriaMirandaShaul","IngaAndreaShaulehijo","",""]
+nombreArchivoHTML1=["","","","",""]
+nombreArchivoHTML2=["","","","",""]
+nombreArchivoHTML3=["","","","",""]
+nombreArchivoHTML4=["","","","",""]
+nombreArchivoHTML5=["","","","",""]
+nombreArchivoHTML6=["","","","",""]  
+"""
+
+
+#for nombre in nombres:
+#    print("./QRs\\"+nombre+".png")
+
+for nombre in nombres1:
+    url ="https://juanpablodeleon.github.io/Invitaciones/"+nombre+".html"
+    file_path = "./QRs\\qr"+nombre+".png"
+
+    qr = qrcode.QRCode()
+    qr.add_data(url)
+
+    img = qr.make_image()
+    img.save(file_path)
+
+    print(url)
+    print("Creado")
+
+"""
+nombr= "invitacion"
+url ="https://juanpablodeleon.github.io/Invitaciones/"+nombr+".html"
+#input("Enter URL: ").strip()
+file_path = "./QRs\\qr"+nombr+".png"
 
 qr = qrcode.QRCode()
 qr.add_data(url)
@@ -9,4 +47,6 @@ qr.add_data(url)
 img = qr.make_image()
 img.save(file_path)
 
+print(url)
 print("Creado")
+"""
