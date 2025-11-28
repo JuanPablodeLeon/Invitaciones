1d3f1a7624fd34b97feb83a01df3da75b1b0fd21
Juan Pablo de León Miranda <JuanPablodeLeon>
2025-11-28 00:38:25 -0600

    Cuarta Tandada

--- imagenQR.py
+++ imagenQR.py
@@ -3,7 +3,7 @@ import qrcode
 nombres1 = ["Lic_Juan_Ramon_y_fam", "Gloria_Shaul", "Ing_Andrea", "Ing_Rodolfo", "Lic_Gabriela"]
 nombres2 = ["Maria_Elena_Shaul", "Sr_Leonardo", "Profe_Rolando", "Lic_Saul", "Inge_Rolando"]
 nombres3 = ["Lic_Manolita", "Profa_Karla", "Sra_Marcela", "Profa_Eunice", "Ing_Ivan"]
-nombres4 = ["Prof_Bernardo", "Anita", "Ing_Miguel", "Javier", "Julio"]
+nombres4 = ["Prof_Bernardo", "Anita_Perez", "Ing_Miguel", "Javier", "Julio"]
 nombres5 = ["Prof_Roberto", "Sharon", "Ing_Mario", "Profa_Anita", "Rony"]
 nombres6 = ["Ing_Carlos", "Scarleth", "Liliana", "Ing_Herman", "Profa_Magnolia"]
 nombres7 = ["Lica_Dedy", "Lica_Evelyn", "Sra_Elena", "Ing_Jovany"]
@@ -18,7 +18,7 @@ for nombre in nombres1:
 #    print("./QRs\\"+nombre+".png")
 
 #Modificar por el numero por cada lista
-for nombre in nombres3:
+for nombre in nombres4:
     url ="https://juanpablodeleon.github.io/Invitaciones/"+nombre+".html"
     file_path = "./QRs\\qr"+nombre+".png"
 
